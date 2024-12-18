fcc79f597057f2dc48eddb6d7089254ebf7b5cdb
Ryan Schafer <ryanschafer@pingidentity.com>
2024-12-17 22:41:51 -0700

    Finishes Section 2: Lesson 9

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+// Section 2: Lesson 9
+// Declare variables called country, continent, and population and assign their values according to your own counry (population in millions).
+// Log their values to the console.
+
+let country = "United States of America";
+let continent = "North America";
+let population = 460000000;
+
+console.log("Country:", country);
+console.log("Continent:", continent);
+console.log("Population:", population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,14 @@
+// Section 2: Lesson 8 ---------------------------
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
-let calculation = 40 + 8 + 23 - 10;
-console.log(calculation);
+// let calculation = 40 + 8 + 23 - 10;
+// console.log(calculation);
+
+// Section 2: Lesson 9 ---------------------------
+console.log("Ryan")
+
+let firstName = "Ryan";
+console.log(firstName);
+// NOTE: You cannot set a variable name to start with a number, & or uses a reserved JS keyword - IE: let 3years = 3;
+// You can also define constants using all uppercase letters, IE: let PI = 3.1415;
