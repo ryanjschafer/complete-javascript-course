65e00a3832b045b629778c04ec8dc5abe404a762
Ryan Schafer <ryanschafer@Ryans-MacBook-Pro.local>
2025-03-03 20:27:03 -0700

    Finished Section 2: Lesson 13

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-// Section 2: Lesson 8 ---------------------------
+// Section 2: Lesson 8 - LINKING A JS FILE ---------------------------
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
 
@@ -7,7 +7,7 @@ let js = "amazing";
 
 // ================================================================
 
-// Section 2: Lesson 9 ---------------------------
+// Section 2: Lesson 9 - VALUES AND VARIABLES ---------------------------
 console.log("Ryan");
 
 let firstName = "Ryan";
@@ -17,7 +17,7 @@ console.log(firstName);
 
 // ================================================================
 
-// Section 2: Lesson 10 ---------------------------
+// Section 2: Lesson 11 - DATA TYPES ---------------------------
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -36,7 +36,7 @@ year = 1991; // Used to be undefined, but redefining to a value (number data typ
 
 // ================================================================
 
-// Section 2: Lesson 11 ---------------------------
+// Section 2: Lesson 12 - LET, CONST, VAR ---------------------------
 let age = 30; // let allows you to "mutate" variable later in the code
 age = 31;
 
@@ -45,3 +45,37 @@ const birthYear = 1995; // this is immmutable and cannot be changed under const
 // const job; this will cause an error, can't be "undefined"
 
 // Avoid using "var" to define variables as it is an old way. Uses pretty much the same way as "let"
+
+// ================================================================
+
+// Section 2: Lesson 13 - BASIC OPERATORS ---------------------------
+// Mathematic operators
+
+const now = 2037;
+const ageRyan = now - 1991;
+const ageJonas = now - 2018;
+console.log(ageRyan, ageJonas);
+
+console.log(ageRyan * 2, ageRyan / 10, 2 ** 3); // You can add multiple math statements in one console.log()
+// 2 ** 3 means 2 to the power of 3 = 2x2x2
+
+const firstNameMe = "Ryan";
+const lastName = "Schafer";
+console.log(firstName + " " + lastName); // This will outpuut "Ryan Schafer"
+
+// Assignment Operators (=)
+let x = 10 + 5; // x will be equal to 15 in this example
+x += 10; // x = x + 10, should now be 25 as we are adding 10 to 15 in line 67
+x *= 4; // x = x * 4 = 100 (still compounting from line 68)
+x++; // x = x + 1 = 101 (added form line 69)
+x--; // equals 100, opposite of ++, decreases value by 1
+console.log(x);
+
+// Comparison Operators (produces boolean operations)
+console.log(ageRyan > ageJonas); // true, as previously defined on lines 55 and 56
+
+console.log(ageJonas >= 18); // true as defined on line 56
+
+const isFullAge = ageJonas >= 18; // Stores line 77 as a variable
+
+consol.log(now - 1991 > now - 2018); // returns true
